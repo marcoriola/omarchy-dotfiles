0f01189426d52088a1969ee8ba0e795daa392741
Marco Riola <131653827+marcoriola@users.noreply.github.com>
2025-10-09 19:22:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cp -r ~/.config/* ~/dotfiles-backup
 cp -rf .config/* ~/.config/
 mv ~/.zshrc ~/.zshrc-backup
 cp -f .zshrc ~/.zshrc
-echo "Installed successfully. Backed up previous dotfiles in ~/dotfiles-backup"
+echo "Installed successfully. Backed up previous dotfiles in ~/dotfiles-backup and ~/.zshrc"
