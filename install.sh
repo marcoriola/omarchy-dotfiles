583e8a00f3f4b38865ad2df69886a327750f7348
Marco Riola <131653827+marcoriola@users.noreply.github.com>
2026-01-25 00:32:52 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-mkdir ~/dotfiles-backup
-cp -r ~/.config/* ~/dotfiles-backup
-cp -rf .config/* ~/.config/
-mv ~/.zshrc ~/.zshrc-backup
-cp -f .zshrc ~/.zshrc
-echo "Installed successfully. Backed up previous dotfiles in ~/dotfiles-backup and ~/.zshrc"
